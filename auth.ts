bcb9fbc611a1a651b4aeb39e334fc6dddfc8e42c
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-19 21:18:22 +0200

    add callbacks to auth.ts

--- auth.ts
+++ auth.ts
@@ -1,11 +1,48 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import { db } from "@/lib/db";
-
 import authConfig from "@/auth.config";
 
+import { db } from "@/lib/db";
+
+import { getUserById } from "@/data/user";
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    // async signIn({ user }) {
+    //   let existingUser;
+
+    //   if (user.id) {
+    //     existingUser = await getUserById(user.id);
+    //   }
+
+    //   if (!existingUser || !existingUser.emailVerified) return false;
+
+    //   return true;
+    // },
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token; // that means the user is logged out
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,21 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+import { UserRole } from "@prisma/client";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  id: string;
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: UserRole;
+  }
+}
