60259831a62dd62a72ffcdeb37e477ab814cb9c2
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-20 18:12:14 +0200

    add reset password feature

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { db } from "@/lib/db";
 
 import { getVerificationTokenByEmail } from "@/data/verification-token";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
@@ -21,4 +22,23 @@ export const generateVerificationToken = async (email: string) => {
   });
 
   return verificationToken;
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  const expires = new Date(new Date().getTime() + 3600 * 1000); // in 1 hour
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: existingToken.id },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: { email, token, expires },
+  });
+
+  return passwordResetToken;
 };
