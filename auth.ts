0d4185368ee61d334bb2f4183cd67883e3d5ae7c
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-21 18:26:29 +0200

    create settings page with update actions

--- auth.ts
+++ auth.ts
@@ -7,6 +7,7 @@ import { db } from "@/lib/db";
 
 import { getUserById } from "@/data/user";
 import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
+import { getAccountByUserId } from "@/data/account";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
@@ -62,15 +63,28 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
 
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
+      console.log("I AM BEING CALLED AGAIN!");
+
       if (!token.sub) return token; // that means the user is logged out
 
       const existingUser = await getUserById(token.sub);
 
       if (!existingUser) return token;
 
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
