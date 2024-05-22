4f68b391be805db11ff216b3c965cd0c69462e63
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2024-05-22 21:37:40 +0200

    add fallback to auth-layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import { Suspense } from "react";
+import { PulseLoader } from "react-spinners";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex items-center justify-center bg-blue-gradient">
-      {children}
+      <Suspense fallback={<PulseLoader color="#fff" />}>{children}</Suspense>
     </div>
   );
 };
